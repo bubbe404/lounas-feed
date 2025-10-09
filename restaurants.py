3ba69aa82ad1859e472da75579c25167d812c453
bubbe404 <max@mclang.com>
2025-10-09 23:37:15 +0300

    Create restaurants.py

---
+++ restaurants.py
@@ -0,0 +1,39 @@
+# restaurants.py
+
+restaurants = [
+    {
+        "name": "Makiata",
+        "url": "https://makiata.fi/lounas",
+        "hours": "11:00–13:00",
+        "prices": {"Soup+salad+coffee": "12,70€", "Full lunch": "13,70€"},
+        "type": "table"
+    },
+    {
+        "name": "Telakka",
+        "url": "https://bistrotelakka.fi/lounas",
+        "hours": "11:00–14:00",
+        "prices": {"Buffet": "13,70€", "A la carte available": "–"},
+        "type": "list"
+    },
+    {
+        "name": "Persilja",
+        "url": "https://persilja.fi/lounas",
+        "hours": "10:30–15:00",
+        "prices": {"Buffet": "13,70€", "Seniors": "12,50€"},
+        "type": "div_snippet"
+    },
+    {
+        "name": "Pisara",
+        "url": "https://pisara.fi/lounas",
+        "hours": "11:00–14:00",
+        "prices": {"Full lunch": "13,00€", "Soup": "11,50€"},
+        "type": "simple_p"
+    },
+    {
+        "name": "Casa Mare",
+        "url": "https://casamare.fi/lounas",
+        "hours": "11:00–14:00",
+        "prices": {"Full lunch": "14,00€"},
+        "type": "table"
+    }
+]
