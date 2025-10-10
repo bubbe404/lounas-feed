9bdd35f6cfed746aa6deea84ddb9787ea28273a6
bubbe404 <max@mclang.com>
2025-10-10 09:23:13 +0300

    Update restaurants.py

--- restaurants.py
+++ restaurants.py
@@ -3,37 +3,37 @@
 restaurants = [
     {
         "name": "Makiata",
-        "url": "https://makiata.fi/lounas",
+        "url": "https://www.makiata.fi/lounas/",
         "hours": "11:00–13:00",
-        "prices": {"Soup+salad+coffee": "12,70€", "Full lunch": "13,70€"},
+        "prices": {"Buffet": "13,70€","Soup": "12,70€"},
         "type": "table"
     },
     {
-        "name": "Telakka",
-        "url": "https://bistrotelakka.fi",
+        "name": "Bistro Telakka",
+        "url": "https://www.bistrotelakka.fi",
         "hours": "11:00–14:00",
-        "prices": {"Buffet": "13,70€", "A la carte available": "–"},
+        "prices": {"Buffet": "13,70€"},
         "type": "list"
     },
     {
         "name": "Persilja",
-        "url": "https://persilja.fi/lounas",
+        "url": "https://www.ravintolapersilja.fi/lounas",
         "hours": "10:30–15:00",
-        "prices": {"Buffet": "13,70€", "Seniors": "12,50€"},
+        "prices": {"Buffet": "13,70€"},
         "type": "div_snippet"
     },
     {
         "name": "Pisara",
         "url": "https://ravintolapisara.fi/lounaslistat/lauttasaari",
-        "hours": "11:00–14:00",
-        "prices": {"Full lunch": "13,00€", "Soup": "11,50€"},
+        "hours": "10:30-14:00",
+        "prices": {"Buffet": "13,00€", "House lunch": "12,00€", "Soup": "11,50€"},
         "type": "simple_p"
     },
     {
         "name": "Casa Mare",
-        "url": "https://ravintolacasamare.fi/lounas",
+        "url": "https://www.ravintolacasamare.com/lounas/",
         "hours": "11:00–14:00",
-        "prices": {"Full lunch": "14,00€"},
+        "prices": {"Buffet": "13,70€"},
         "type": "table"
     }
 ]
